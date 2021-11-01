f7e6541afb58690f5d43807b92b8911e4f80b2d0
Inaê Rosa <inaedb.rosa@gmail.com>
2021-11-01 20:43:27 -0300

    category and product linking

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express();
 const data = require('./data.json')
 
 app.get('/', (req, res) => {
-    res.json(data);
+    let products = data.products;
+    let category = data.categories;
+
+        for (var i = 0; i < products.length; i ++){
+            let catP = products[i].categoriesId;
+            for (var j = 0; j < category.length; j++){
+                for (var k = 0; k < catP.length; k++){
+                    if (category[j].id == catP[k]){
+                         console.log("Product: " + products[i].name);
+                         console.log("Category: " + category[j].name)
+                     }
+                }   
+            }
+        }
 })
 
 app.listen(3000, () => {
