cb84c8de82c9d60896844272eb8e1b206d86cc8f
Inaê Rosa <inaedb.rosa@gmail.com>
2021-11-02 23:07:38 -0300

    json structure config

--- app.js
+++ app.js
@@ -17,28 +17,28 @@ app.get('/', (req, res) => {
     let products = data.products;
     let category = data.categories;
     let establishments = data.establishments;
+    var myEst = new Object();
     let array = []
+
     for (var l = 0; l < establishments.length; l++){
+        myEst[establishments[l].name] = {}
         let prodId = establishments[l].productsId;
         for (var i = 0; i < products.length; i ++){
             let catP = products[i].categoriesId;
             for (var j = 0; j < category.length; j++){
                 for (var k = 0; k < catP.length; k++){
                     for (var m = 0; m < prodId.length; m++){
-                        if (category[j].id == catP[k] && products[i].id== prodId[m]){
-                            var obj = {
-                                establishments:establishments[l].name,category: category[j].name, products: {product: products[i].name, price: products[i].price/100}
-                            }
-                            array.push(obj) 
+                        if (category[j].id == catP[k] &&products[i].id== prodId[m]){
+                            myEst[establishments[l].name][products[i].name] = {price: products[i].price/100}
+                            JSONWrite('./output.json', myEst).then(console.log).catch(console.error)
                         }
-                        
-                    }           
-                }    
-            }   
+                    }        
+                }   
+            }
         }
-       
+        array.push(myEst)
     }
-        res.json(array)
+    res.json(myEst)
 })
 
 
