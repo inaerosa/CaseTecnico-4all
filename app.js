5f4c5f64571d089bf77787d7942700d4e5d2142b
Inaê Rosa <inaedb.rosa@gmail.com>
2021-11-02 23:03:12 -0300

    JSONWrite for output.json

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
 const data = require('./data.json')
+
+const JSONWrite = (filePath, data, encoding = 'utf-8') => {
+    const promiseCallback = (resolve, reject) => {
+        fs.writeFile(filePath, JSON.stringify(data, null, 2), encoding, (err) => {
+            if (err) return reject(err)
+            resolve(true)
+        })
+    }
+    return new Promise(promiseCallback)
+}
 
 app.get('/', (req, res) => {
     let products = data.products;
@@ -17,8 +28,6 @@ app.get('/', (req, res) => {
                         if (category[j].id == catP[k] && products[i].id== prodId[m]){
                             var obj = {
                                 establishments:establishments[l].name,category: category[j].name, products: {product: products[i].name, price: products[i].price/100}
-                                
-                               
                             }
                             array.push(obj) 
                         }
@@ -31,6 +40,8 @@ app.get('/', (req, res) => {
     }
         res.json(array)
 })
+
+
 
 app.listen(3000, () => {
     console.log("Running on port 3000")
